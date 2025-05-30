79156c86d4e5f4fc0c1bba111d91c6c9f3feaf0f
Taher Farh <taher.farh9@gmail.com>
2025-05-30 19:52:47 +0300

    Delete Downloads/hamza-main/hamza-main directory

--- Downloads/hamza-main/hamza-main/lib/Features/video List/cubit/List_States.dart
+++
@@ -1,9 +0,0 @@
-class ListStates {}
-
-class InitListStates extends ListStates{}
-
-class LoadingListStates extends ListStates{}
-
-class SuccessfulListStates extends ListStates{}
-
-class ErrorListStates extends ListStates{}
--- Downloads/hamza-main/hamza-main/lib/core/constant.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
- Color kprimecolor=Color.fromARGB(255, 2, 146, 131);
